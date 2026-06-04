3634a62219cc611ff5b8b57b3186435159db55e9
Studente Cerseo <esame@cerseo.it>
2026-06-04 11:18:46 +0200

    Modifice

--- scripts/services/api.js
+++ scripts/services/api.js
@@ -56,6 +56,21 @@ async function requestJson(url, errorPrefix) {
     // Poi in caso di errore rella risposta, mandare un messaggio di errore che contenga il prefisso errorPrefix e l'eventuale messaggio di errore restituito dalla fetch
     // Infine restituisci i dati parsati come oggetto, senza manipolarli o trasformarli
     // Controlla sempre anche errori di rete o altri errori imprevisti con un catch e restituisci un messaggio di errore coerente con il prefisso
+    const url = ${ API_BASE }/topstories.json;
+
+    try {
+        const data = await requestJson(url, "Errore di recupero");
+        if (!data) {
+            return [];
+        }
+        return data;
+
+    } catch {
+        console.error();
+        return [];
+
+
+    }
 }
 
 /**
